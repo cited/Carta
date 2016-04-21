3ddc135071f62bff31094dd8bcdac52726efd012
DavidGhedini <david@davidghedini.com>
2016-04-21 21:06:27 +0200

    Carta Layer

---
+++ models/CartaLayer.php
@@ -0,0 +1,13 @@
+<?php
+
+class CartaLayer extends Omeka_Record_AbstractRecord
+{
+    public $id;
+    public $name;
+    public $width;
+    public $height;
+    public $baselayer;
+    public $layergroup;
+    public $pointers;
+    public $type;   
+}
