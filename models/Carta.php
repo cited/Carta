9b869f80939df30db4bf04b0547bd462d4d31e9d
DavidGhedini <david@davidghedini.com>
2016-04-21 21:05:56 +0200

    Carta Model

---
+++ models/Carta.php
@@ -0,0 +1,14 @@
+<?php
+
+class Carta extends Omeka_Record_AbstractRecord
+{
+    public $id;
+    public $name;
+    public $width;
+    public $height;
+    public $baselayer;
+    public $layergroup;
+    public $zoom;   
+    public $pointers;    
+   
+}
