424ba4eaa5af792a4dee14ce8042206768f7f18a
DavidGhedini <david@davidghedini.com>
2016-05-05 23:32:17 +0200

    shortcode

--- shortcode.php
+++ shortcode.php
@@ -92,8 +92,11 @@ $id=$jId;
   <link href='https://cdn.mapfig.com/mapfig-cdn/L.Control.Locate.ie.css' rel='stylesheet' />
 <![endif]-->
 
+<script src='https://cdn.mapfig.com/mapfig-cdn/leaflet.markercluster.js'></script>
+<link href='https://cdn.mapfig.com/mapfig-cdn/MarkerCluster.css' rel='stylesheet' />
+<link href='https://cdn.mapfig.com/mapfig-cdn/MarkerCluster.Default.css' rel='stylesheet' />
+
 <link href='<?php echo admin_url("../") ?>plugins/Carta/css/carta.css' rel='stylesheet' />
-<script type="text/javascript" src="<?php echo admin_url("../") ?>plugins/Carta/js/modal.js"></script>
 <?php  endif; ?>
 
 <textarea style="display:none;" id="geo_image_olverlays_<?php echo $id;?>"><?php echo ($carta->geo_image_olverlays) ? base64_decode($carta->geo_image_olverlays) : "[]"; ?></textarea>
@@ -103,24 +106,30 @@ $id=$jId;
 var mbAttribution_<?php echo $id;?>=' contributors | <a href="https://www.mapfig.com" target="_blank">Mapfig</a>';var show_sidebar_<?php echo $id;?>=<?php echo ($carta->show_sidebar) ? "true" : "false"; ?>;var mapOverlays_<?php echo $id;?>=new L.LayerGroup();
 var defaultLayer_<?php echo $id;?>=<?php echo $defaultLayer;?>;
 var defaultLayerMiniMap_<?php echo $id;?>=<?php echo $defaultLayer;?>;
-var featureGroup_<?php echo $id;?>=new L.FeatureGroup();var map_scale_<?php echo $id;?>=L.control.scale({position:'bottomleft',maxWidth:100,metric:true,imperial:true,updateWhenIdle:false});var baseLayers_<?php echo $id;?>={<?php echo $baseLayers?>};var overlays_<?php echo $id;?>={"Map Points":featureGroup_<?php echo $id;?>};var layerSelector_<?php echo $id;?>=L.control.layers(baseLayers_<?php echo $id;?>,overlays_<?php echo $id;?>);map_<?php echo $id;?>=new L.Map("map_<?php echo $id; ?>",{dragging:true,touchZoom:true,scrollWheelZoom:true,doubleClickZoom:true,boxzoom:true,trackResize:true,worldCopyJump:false,closePopupOnClick:true,keyboard:true,keyboardPanOffset:80,keyboardZoomOffset:1,inertia:true,inertiaDeceleration:3000,inertiaMaxSpeed:1500,zoomControl:true,crs:L.CRS.EPSG3857,layers:[defaultLayer_<?php echo $id;?>,featureGroup_<?php echo $id;?>]});map_<?php echo $id;?>.setView([<?php echo $first_lat;?>,<?php echo $first_lng;?>],<?php echo $carta->zoom;?>);L.control.fullscreen().addTo(map_<?php echo $id;?>);
+var featureGroup_<?php echo $id;?> = (<?PHP echo ($carta->show_cluster) ? "true" : "false"; ?>) ? new L.MarkerClusterGroup() : new L.FeatureGroup();
+//var featureGroup_<?php echo $id;?>=new L.FeatureGroup();
+var map_scale_<?php echo $id;?>=L.control.scale({position:'bottomleft',maxWidth:100,metric:true,imperial:true,updateWhenIdle:false});var baseLayers_<?php echo $id;?>={<?php echo $baseLayers?>};var overlays_<?php echo $id;?>={"Map Points":featureGroup_<?php echo $id;?>};var layerSelector_<?php echo $id;?>=L.control.layers(baseLayers_<?php echo $id;?>,overlays_<?php echo $id;?>);map_<?php echo $id;?>=new L.Map("map_<?php echo $id; ?>",{dragging:true,touchZoom:true,scrollWheelZoom:true,doubleClickZoom:true,boxzoom:true,trackResize:true,worldCopyJump:false,closePopupOnClick:true,keyboard:true,keyboardPanOffset:80,keyboardZoomOffset:1,inertia:true,inertiaDeceleration:3000,inertiaMaxSpeed:1500,zoomControl:true,crs:L.CRS.EPSG3857,layers:[defaultLayer_<?php echo $id;?>,featureGroup_<?php echo $id;?>]});map_<?php echo $id;?>.setView([<?php echo $first_lat;?>,<?php echo $first_lng;?>],<?php echo $carta->zoom;?>);L.control.fullscreen().addTo(map_<?php echo $id;?>);
 L.control.locate({position:'bottomright',drawCircle:true,follow:true,setView:true,keepCurrentZoomLevel:true,remainActive:false,circleStyle:{},markerStyle:{},followCircleStyle:{},followMarkerStyle:{},icon:'icon-cross-hairs',circlePadding:[0,0],metric:true,showPopup:true,strings:{title:'I am Here',popup:'You are within {distance} {unit} from this point',outsideMapBoundsMsg:'You seem located outside the boundaries of the map'},locateOptions:{watch:true}}).addTo(map_<?php echo $id;?>);
 featureGroup_<?php echo $id;?>.addTo(map_<?php echo $id;?>);
 map_scale_<?php echo $id;?>.addTo(map_<?php echo $id;?>);
-var jsonData_<?php echo $id;?>=L.geoJson(null,{style:function(feature){return{color:"#f06eaa","weight":4,"opacity":0.5,"fillOpacity":0.2};},onEachFeature:function(feature,layer){featureGroup_<?php echo $id;?>.addLayer(layer);properties1=feature.properties;var properties=new Array();for(var i=0;i<properties1.length;i++){row={};row['name']=properties1[i].name;row['value']=properties1[i].value;row['defaultProperty']=properties1[i].defaultProperty;properties.push(row);}
-layerProperties.push(new Array(layer,properties));console.log(properties);var style=feature.style;var cp=feature.customProperties;if(style){if(layer instanceof L.Marker){if(style.markerColor){layer.setIcon(L.AwesomeMarkers.icon(style));}}
+var jsonData_<?php echo $id;?>=L.geoJson(null,{style:function(feature){return{color:"#f06eaa","weight":4,"opacity":0.5,"fillOpacity":0.2};},onEachFeature:function(feature,layer){featureGroup_<?php echo $id;?>.addLayer(layer);
+layer.on("click", function(){
+	openPopup_<?php echo $id; ?>(layer);
+});
+properties1=feature.properties;var properties=new Array();for(var i=0;i<properties1.length;i++){row={};row['name']=properties1[i].name;row['value']=properties1[i].value;row['defaultProperty']=properties1[i].defaultProperty;properties.push(row);}
+layerProperties_<?php echo $id; ?>.push(new Array(layer,properties));var style=feature.style;var cp=feature.customProperties;if(style){if(layer instanceof L.Marker){if(style.markerColor){layer.setIcon(L.AwesomeMarkers.icon(style));}}
 else{layer.setStyle(style);}}
-shapeStyles.push(style);shapeCustomProperties.push(cp);renderSideBar_<?php echo $id;?>(layer);bindPopup(layer);}})
+shapeStyles_<?php echo $id; ?>.push(style);shapeCustomProperties_<?php echo $id; ?>.push(cp);renderSideBar_<?php echo $id;?>(layer);bindPopup_<?php echo $id; ?>(layer);}})
 jQuery('#map_<?php echo $id; ?> .leaflet-top.leaflet-left').append('<div id="sidebarhideshow_<?php echo $id; ?>" class="leaflet-control-sidebar leaflet-bar leaflet-control" style="z-index:10;">'+'<a class="leaflet-control-sidebar-button leaflet-bar-part" id="sidebar-button-reorder_<?php echo $id; ?>" href="#" onClick="return false;" title="Sidebar Toggle"><i class="fa fa-reorder"></i></a>'+'<div id="sidebar-buttons_<?php echo $id; ?>" class="sidebar-buttons" style="max-height: 300px; overflow: auto;">'+'<ul class="list-unstyled leaflet-sidebar">'+'</ul>'+'</div>'+'</div>');
 jQuery('#map_<?php echo $id; ?> .leaflet-top.leaflet-left').append('<div id="edit_image_overlays_<?php echo $id; ?>" class="leaflet-control-sidebar leaflet-bar leaflet-control" style="z-index:10;"><a class="leaflet-control-sidebar-button leaflet-bar-part" href="#" onclick="return false;" title="Set Image Overlay Opacity"><i class="fa fa-image"></i></a></div>');
 function renderSideBar_<?php echo $id;?>(layer){if(show_sidebar_<?php echo $id;?>)
-jQuery('#sidebarhideshow_<?php echo $id; ?>').show();else{jQuery('#sidebarhideshow_<?php echo $id; ?>').hide();}
-target=jQuery('#sidebar-buttons_<?php echo $id; ?> ul.leaflet-sidebar');currentIndex=getLayerIndex(layer);lable=layerProperties[currentIndex][1][0].value;if(lable==""){lable="No Location";}
+jQuery('#sidebarhideshow_<?php echo $id; ?>').show();else{jQuery('#sidebarhideshow_<?php echo $id; ?>').hide();} if(!layer) return;
+target=jQuery('#sidebar-buttons_<?php echo $id; ?> ul.leaflet-sidebar');currentIndex=getLayerIndex_<?php echo $id; ?>(layer);lable=layerProperties_<?php echo $id; ?>[currentIndex][1][0].value;if(lable==""){lable="No Location";}
 target.append('<li><input type="checkbox" data-index="'+currentIndex+'" onClick="changeAddressCheckbox_<?php echo $id; ?>(this)" checked><a data-index="'+currentIndex+'" onClick="clickOnSidebarAddress_<?php echo $id; ?>(this)">'+lable+'</a><div class="clear"></div></li>');}
 var animating=false;jQuery('#sidebar-button-reorder_<?php echo $id; ?>').click(function(){if(animating)return;var element=jQuery('#sidebar-buttons_<?php echo $id; ?>');animating=true;if(element.css('left')=='-50px'){element.show();element.animate({opacity:'1',left:'0px'},400,function(){animating=false;});}
-else{element.animate({opacity:'0',left:'-50px'},400,function(){animating=false;element.hide();});}});function changeAddressCheckbox_<?php echo $id;?>(obj){var layers=getLayers();console.log(layers);index=jQuery(obj).attr("data-index");console.log(index);if(jQuery(obj).is(':checked')){featureGroup_<?php echo $id;?>.addLayer(layers[index]);}
+else{element.animate({opacity:'0',left:'-50px'},400,function(){animating=false;element.hide();});}});function changeAddressCheckbox_<?php echo $id;?>(obj){var layers=getLayers_<?php echo $id; ?>();console.log(layers);index=jQuery(obj).attr("data-index");console.log(index);if(jQuery(obj).is(':checked')){featureGroup_<?php echo $id;?>.addLayer(layers[index]);}
 else{featureGroup_<?php echo $id;?>.removeLayer(layers[index]);}}
-function clickOnSidebarAddress_<?php echo $id;?>(obj){var layers=getLayers();index=jQuery(obj).attr("data-index");setTimeout(function(){layers[index].openPopup();},50);}
+function clickOnSidebarAddress_<?php echo $id;?>(obj){var layers=getLayers_<?php echo $id; ?>();index=jQuery(obj).attr("data-index");setTimeout(function(){layers[index].openPopup();},50);}
 
 <?php if ($carta->show_minimap) : ?>
 new L.Control.MiniMap(defaultLayerMiniMap_<?php echo $id;?>, {toggleDisplay: true}).addTo(map_<?php echo $id;?>)._minimize(true);
@@ -138,7 +147,7 @@ setTimeout(function() {
 	var legendContent_<?php echo $id;?>   = jQuery("#textarea_legend_content_<?php echo $id;?>").html();
 	
 	jQuery(document).ready(function() {
-		jQuery('.leaflet-objects-pane img').css('background', 'transparent');
+		renderSideBar_<?php echo $id;?>();
 		
 		jQuery('#map_<?php echo $id;?>').append('\
 			<div class="overlay_container" style="z-index: 2; position: absolute; right: 0; left: 0; bottom: 10px;">\
@@ -421,13 +430,222 @@ a.trigger:hover {
 div.overlay_container {
     opacity: 0.75;
 }
+#map_<?php echo $id;?> img {
+	background: transparent !important;
+}
 </style>
+
+
+
+
+
+
+
+
+<script>
+	function staticSidebarPopupResize_<?php echo $id;?>() {
+		m_height = jQuery("#map_<?php echo $id;?>").height();
+		height = m_height-40;
+		
+		jQuery('#static-popup_<?php echo $id;?>').css('max-height',height).css('min-width',250);
+	}
+	
+	jQuery(document).ready(function($) {
+		jQuery("#map_<?php echo $id;?>").append('\
+			<div class="bubble static bound selected" id="static-popup_<?php echo $id;?>" style="display: none; font-size: 14px;">\
+				<a name="close" class="close" id="static-popup-close_<?php echo $id;?>" onClick="mapClosePopup_<?php echo $id;?>();"><i class="fa fa-close"></i></a>\
+				<div class="content body" rv-html="record:body" rv-show="record:body" id="static-popup-content_<?php echo $id;?>"></div>\
+			</div>\
+		');
+		staticSidebarPopupResize_<?php echo $id;?>();
+	});
+</script>
+<style>
+.close {
+    float: right;
+    font-size: 21px;
+    font-weight: bold;
+    line-height: 1;
+    color: #000000;
+    text-shadow: 0 1px 0 #ffffff;
+    opacity: 0.2;
+    filter: alpha(opacity=20);
+}
+.close:hover, .close:focus {
+    color: #000000;
+    text-decoration: none;
+    cursor: pointer;
+    opacity: 0.5;
+    filter: alpha(opacity=50);
+}
+.bubble.static {
+	z-index: 1005;
+	overflow-y: auto;
+	position: absolute;
+	background: #fff;
+	border-radius: 2px;
+	color: #000;
+	padding: 14px;
+	max-height: 90%;
+	max-width: 400px;
+	left: 70px;
+	top: 20px;
+	opacity: .85;
+}
+.bubble.static.selected {
+	opacity: .9;
+}
+.bubble.static.bound {
+	display: block;
+}
+.bubble.static .title {
+	display: inline;
+	font-size: 16px;
+	line-height: 1em;
+}
+.bubble.static .content {
+	margin-top: .7em;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+<script>
+var layerProperties_<?php echo $id; ?> = new Array();
+var shapeStyles_<?php echo $id; ?> = new Array();
+var shapeCustomProperties_<?php echo $id; ?> = new Array();
+
+function getPropertiesByLayer_<?php echo $id; ?>(layer) {
+    for (i = 0; i < layerProperties_<?php echo $id; ?>.length; i++) {
+        if (layerProperties_<?php echo $id; ?>[i][0] == layer) {
+            return layerProperties_<?php echo $id; ?>[i][1];
+        }
+    }
+    return {};
+}
+
+function bindPopup_<?php echo $id; ?>(layer) {
+    popupContent = getPopupContent_<?php echo $id; ?>(layer)
+    layer.bindPopup(popupContent);
+}
+
+function getPopupContent_<?php echo $id; ?>(layer) {
+    popupContent = "";
+    properties = getPropertiesByLayer_<?php echo $id; ?>(layer);
+    customProperties = getCustomPropertiesByLayer_<?php echo $id; ?>(layer);
+    jQuery.each(properties, function(index, property) {
+        if (!customProperties.hide_label) {
+            var label = property.name;
+            if (property.name == "Name") {
+                label = "Location";
+            }
+            popupContent += '<b>' + label + '</b> : ';
+        }
+        if (customProperties.show_address_on_popup) {
+            if (property.name == "Name") {
+				if(customProperties.bootstrap_popup) {
+					popupContent += '<b class="title" id="static-popup-title">' + property.value + '</b><br/>';
+				}
+				else {
+					popupContent += '<b>' + property.value + '</b><br/>';
+				}
+                return true;
+            }
+        }
+        if(property.name != "Name") {
+			popupContent += property.value+'<br/>';
+		}
+    });
+    if (customProperties.get_direction) {
+        jQuery.each(properties, function(index, property) {
+            if (property.name == "Name") {
+                address = property.value;
+                popupContent += '<a href="https://www.google.com/maps/dir//' + address + '" target="_blank">Get Direction</a>';
+                return false;
+            }
+        });
+    }
+    return popupContent;
+}
+
+function getCustomPropertiesByLayer_<?php echo $id; ?>(layer) {
+    for (i = 0; i < layerProperties_<?php echo $id; ?>.length; i++) {
+        if (layerProperties_<?php echo $id; ?>[i][0] == layer) {
+            return shapeCustomProperties_<?php echo $id; ?>[i];
+        }
+    }
+    return {};
+}
+
+function getLayerIndex_<?php echo $id; ?>(layer) {
+    for (i = 0; i < layerProperties_<?php echo $id; ?>.length; i++) {
+        if (layerProperties_<?php echo $id; ?>[i][0] == layer) {
+            return i;
+        }
+    }
+}
+
+function getLayers_<?php echo $id; ?>() {
+    layers = new Array();
+    for (i = 0; i < layerProperties_<?php echo $id; ?>.length; i++) {
+        layers.push(layerProperties_<?php echo $id; ?>[i][0]);
+    }
+    return layers;
+}
+
+function clickOnSidebarAddress_<?php echo $id; ?>(obj) {
+    var layers = getLayers_<?php echo $id; ?>();
+    index = jQuery(obj).parent().index();
+    setTimeout(function() {
+        layers[index].openPopup();
+        openPopup_<?php echo $id; ?>(layers[index]);
+    }, 50);
+}
+
+function openPopup_<?php echo $id; ?>(layer) {
+	mapClosePopup_<?php echo $id; ?>();
+    index = getLayerIndex_<?php echo $id; ?>(layer);
+	
+    if (shapeCustomProperties_<?php echo $id; ?>[index].bootstrap_popup) {
+        setTimeout(function() {
+            map_<?php echo $id; ?>.closePopup();
+            mapOpenPopup_<?php echo $id; ?>(layer);
+        }, 50);
+    }
+}
+
+function mapClosePopup_<?php echo $id; ?>() {
+	jQuery('#static-popup_<?php echo $id; ?>').fadeOut();
+}
+function mapOpenPopup_<?php echo $id; ?>(layer) {
+	popupContent = getPopupContent_<?php echo $id; ?>(layer)
+	jQuery('#static-popup-content_<?php echo $id; ?>').html(popupContent);
+	jQuery('#static-popup_<?php echo $id; ?>').fadeIn();
+}
+</script>
+
+
+
+
+
+
+
+
+
 
 
 
 <?php if (!$notFirstmap) :?>
 
-<script type="text/javascript" src="<?php echo admin_url("../") ?>plugins/Carta/js/helper.js"></script>
+<!-- <script type="text/javascript" src="<?php echo admin_url("../") ?>plugins/Carta/js/helper.js"></script>-->
 
 <?php endif; ?>
 <script type="text/javascript">
